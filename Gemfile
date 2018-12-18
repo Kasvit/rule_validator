a36b0de0387aaa5ad8fbe80037e8bae4a5920bb5
Kasvit <kasvit93@gmail.com>
2018-12-18 09:36:42 -0600

    mix with tree

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source 'https://rubygems.org'
 gem 'pry'
 
 gem 'rack-test'
-
 gem 'mongo'
 gem "redis"
--- test/redis_storage_test.rb
+++ test/redis_storage_test.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/storages/redis_storage'
 require 'minitest/autorun'
-
+require "pry"
 class RedisStorageTest < Minitest::Test
   def setup
     @storage = RedisStorage.new
@@ -14,5 +14,17 @@ class RedisStorageTest < Minitest::Test
   def test_true_on_rule_second_resourse_in_storage
     rule = @storage.find_rule('second_resourse')
     assert_equal rule['resource'], 'second_resourse'
+  end
+
+  def test_true_on_add_new_resourse_in_storage
+    @storage.add_rule('new_resourse', { resource: 'new_resourse',
+                                        description: 'allow access to second resourse',
+                                        params: { id: 123, bp_id: 'abc123', report_id: 128 },
+                                        participant: {id: 123},
+                                        methods: 'GET',
+                                        action:  'allow' })
+
+    rule = @storage.find_rule('new_resourse')
+    assert_equal rule['resource'], 'new_resourse'
   end
 end
