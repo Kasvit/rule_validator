3780913148a05e335b81ed5780b29bb7087f20de
Vadym Zakharchenko <vadim.if@gmail.com>
2018-12-26 13:32:35 +0200

    add exceptions

--- lib/rule_validator_middleware/db_factory.rb
+++ lib/rule_validator_middleware/db_factory.rb
@@ -1,6 +1,9 @@
+require_relative 'exceptions'
 # DataBase factory
 class DbFactory
   def self.db(db = 'yaml')
     Object.const_get("Storages::#{db.capitalize}").new
+    rescue NameError
+      raise Exceptions::InvalidStorage, "invalid storage #{db.capitalize}"
   end
 end
